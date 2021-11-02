3e86aa759948751406d372a88a6ccd449de68046
Veeeeti <59977109+Veeeeti@users.noreply.github.com>
2021-11-02 15:30:34 +0200

    osa11 teht 11.6 fix

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
+/* eslint-disable linebreak-style */
 const express = require('express')
 const app = express()
-
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log('server started on port 5000')
